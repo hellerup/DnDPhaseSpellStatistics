acf5c015a975f7677669de085d2792a31eea974d
Martin Hellerup Madsen <mhm@securespectrum.dk>
2022-11-14 07:25:22 +0100

    More, more, more

--- phase_spell_stats.rb
+++ phase_spell_stats.rb
@@ -3,6 +3,8 @@ def format(value)
 end
 
 # Dumme møgspell
+# Vi registrerer et antal dage med et vist antal kampe og et vist antal attacks mod pc'en pr. runde
+# Vi registrerer alle attacks og antallet af 'Ville have ramt'
 examples = 10
 armor_class = 16
 attack_bonus = 4
@@ -25,45 +27,46 @@ examples.times do |d|
     combat = c + 1
     combat_rounds.times do |r|
       round = r + 1
-      shield_used = false # Effective against all attacks in this round
+      shield_active = false # Effective against all attacks in this round
       attacks_pr_round.times do |att|
+
         attack_no = att + 1
         id = "#{day}-#{combat}-#{round}-#{attack_no}".to_sym
         primary_roll = rand(1..20)
         second_roll = rand(1..20)
         disadv_roll = [primary_roll, second_roll].min
-        shield_bonus = (current_number_of_shield_spells.positive? ? 5 : 0)
+        shield_bonus = (current_number_of_shield_spells.positive? || shield_active ? 5 : 0)
 
         # Will the primary attack hit
         primary_hits = primary_roll + attack_bonus >= armor_class
+        
+        # And Phase
         dis_adv_hits = disadv_roll + attack_bonus >= armor_class
+
+        # We need to register all attacks where the Shield prevents the attack
         will_shield_prevent_hit = primary_hits && primary_roll + attack_bonus >= armor_class + shield_bonus
+        shield_active = true if primary_hits && will_shield_prevent_hit
 
-        # If this is the first round and if the dis_adv hit hits, next round will be skipped
-        # We simply wont calculate damage for this round in this case
-        phase_hit = if attack_no == 1 && dis_adv_hits
-
-        hit = false
-        hit = true if attack_no == 1 && dis_adv_hits
-        hit = true if primary_hits && !will_shield_prevent_hit
+        # If this is the first attack and if the dis_adv hit hits, next round will be skipped
+        phase_hit = true if attack_no == 1 && dis_adv_hits
 
         damage_dealt = rand(1..damage_die) + damage_die_bonus
-        shield_used = shield_hit if shield_hit && !shield_used
+        # shield_active = shield_hit if shield_hit && !shield_active
 
         rolls[id] = {
           id: id,
           roll: primary_roll,
           second_roll: second_roll,
           disadv_roll: disadv_roll,
-          hit: hit,
-          damage_dealt: damage_dealt
+          phase_hit: phase_hit,
+          damage_dealt: damage_dealt,
         }
 
         puts rolls[id] if print
       end
 
       # Remove a shield charge if used in the round
-      current_number_of_shield_spells -= 1 if shield_used
+      current_number_of_shield_spells -= 1 if shield_active
     end
   end
 end
@@ -78,8 +81,6 @@ damage_dealt = 0
 rolls.each do |_k, v|
   phase_hits += 1 if v[:phase_hit]
   shield_hits += 1 if v[:shield_hit]
-  second_phase_hit += 1 if v[:second_attack_hit]
-  second_shield_hit += 1 if v[:second_attack_shield_hit]
   damage_dealt_phase += v[:damage_dealt] unless v[:skip_round]
   damage_dealt += v[:damage_dealt]
 end
@@ -95,9 +96,6 @@ puts "Egen damage pr. runde #{number_of_damage_dice}d#{damage_die}+#{damage_die_
 puts ''
 puts "Antal hits mod Phase (og dermed skippede runder): #{phase_hits}"
 puts "Antal hits mod Shield: #{shield_hits}"
-puts ''
-puts "Antal samlet antal hits ved brug af Phase: #{phase_hits + second_phase_hit}"
-puts "Antal samlet antal hits ved brug af Phase: #{shield_hits + second_shield_hit}"
 puts ''
 puts "Gennemsnitligt Phase Damage output pr. dag: #{format(damage_dealt_phase / examples)}"
 puts "Gennemsnitligt Damage output pr dag: #{format(damage_dealt / examples)}"
