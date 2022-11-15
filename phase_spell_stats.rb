9f6c6a9cf469f39c0292dad7e8297548b8b0a5c0
Martin Hellerup Madsen <mhm@securespectrum.dk>
2022-11-15 07:27:40 +0100

    More

--- phase_spell_stats.rb
+++ phase_spell_stats.rb
@@ -25,11 +25,13 @@ examples.times do |d|
   current_number_of_shield_spells = number_of_shield_spells
   combats_pr_day.times do |c|
     combat = c + 1
+    phase_hit = false
     combat_rounds.times do |r|
       round = r + 1
       shield_active = false # Effective against all attacks in this round
+      skip_damage = phase_hit
+      phase_hit = false
       attacks_pr_round.times do |att|
-
         attack_no = att + 1
         id = "#{day}-#{combat}-#{round}-#{attack_no}".to_sym
         primary_roll = rand(1..20)
@@ -39,20 +41,23 @@ examples.times do |d|
 
         # Will the primary attack hit
         primary_hits = primary_roll + attack_bonus >= armor_class
-        
-        # And Phase
+
+        # Phase
+        # Only relevant on attack 1
         dis_adv_hits = disadv_roll + attack_bonus >= armor_class
+        # If this is the first attack and if the dis_adv hit hits, next round will be skipped
+        phase_hit = true if attack_no == 1 && dis_adv_hits
 
         # We need to register all attacks where the Shield prevents the attack
         will_shield_prevent_hit = primary_hits && primary_roll + attack_bonus >= armor_class + shield_bonus
         shield_active = true if primary_hits && will_shield_prevent_hit
 
-        # If this is the first attack and if the dis_adv hit hits, next round will be skipped
-        phase_hit = true if attack_no == 1 && dis_adv_hits
-
-        damage_dealt = rand(1..damage_die) + damage_die_bonus
-        # shield_active = shield_hit if shield_hit && !shield_active
-
+        if skip_damage
+          damage_dealt = 0
+        else
+          damage_dealt = rand(1..damage_die) + damage_die_bonus 
+        end
+        
         rolls[id] = {
           id: id,
           roll: primary_roll,
@@ -81,7 +86,7 @@ damage_dealt = 0
 rolls.each do |_k, v|
   phase_hits += 1 if v[:phase_hit]
   shield_hits += 1 if v[:shield_hit]
-  damage_dealt_phase += v[:damage_dealt] unless v[:skip_round]
+  damage_dealt_phase += v[:damage_dealt]
   damage_dealt += v[:damage_dealt]
 end
 
